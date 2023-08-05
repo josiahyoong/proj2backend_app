6f719fc76eb9f7579764251ede0a307070367f6b
josiahyoong <josiahyoong@gmail.com>
2023-08-05 22:03:25 +0800

    Setup Node.js and Express.js

---
+++ server.js
@@ -0,0 +1,13 @@
+// backend/server.js
+
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+app.get('/', (req, res) => {
+  res.send('Backend is running!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
